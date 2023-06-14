8f6dbc68478dd10f99bf904f703b21fc9d158388
Petter Berntsson <petter.berntsson@gmail.com>
2023-06-14 19:31:13 +0200

    Only center around first face detected

--- main.py
+++ main.py
@@ -19,28 +19,29 @@ def center_images(image_files, input_folder, output_folder):
 
         # Detect faces in the image
         faces = dlib.get_frontal_face_detector()(gray)
+        # Take first face only as the first detected face is
+        #  most likely the person who is taking the selfie
+        face = faces[0]
 
-        for face in faces:
-            # Detect facial landmarks including the nose
-            landmarks = predictor(gray, face)
-            nose_landmark = landmarks.part(30)  # Index 30 corresponds to the tip of the nose
+        # Detect facial landmarks including the nose
+        landmarks = predictor(gray, face)
+        nose_landmark = landmarks.part(30)  # Index 30 corresponds to the tip of the nose
 
-            # Calculate the offset for centering the image around the nose
-            dx = int(img.shape[1] / 2 - nose_landmark.x)
-            dy = int(img.shape[0] / 2 - nose_landmark.y)
+        # Calculate the offset for centering the image around the nose
+        dx = int(img.shape[1] / 2 - nose_landmark.x)
+        dy = int(img.shape[0] / 2 - nose_landmark.y)
 
-            # Create a translation matrix to shift the image
-            translation_matrix = np.float32([[1, 0, dx], [0, 1, dy]])
+        # Create a translation matrix to shift the image
+        translation_matrix = np.float32([[1, 0, dx], [0, 1, dy]])
 
-            # Apply the translation to center the image around the nose
-            centered_img = cv2.warpAffine(img, translation_matrix, (img.shape[1], img.shape[0]))
+        # Apply the translation to center the image around the nose
+        centered_img = cv2.warpAffine(img, translation_matrix, (img.shape[1], img.shape[0]))
 
-            # Save the centered image with a new filename
-            centered_filename = "centered_" + str(image_file).replace(input_folder + "/", "")
-            cv2.imwrite(output_folder + "/" + centered_filename, centered_img)
+        # Save the centered image with a new filename
+        centered_filename = "centered_" + str(image_file).replace(input_folder + "/", "")
+        cv2.imwrite(output_folder + "/" + centered_filename, centered_img)
 
-            print(f"{current_image}/{total_images} - './{image_file}' centered around the nose & saved as './{output_folder}/{centered_filename}'")
-            break
+        print(f"{current_image}/{total_images} - './{image_file}' centered around the nose & saved as './{output_folder}/{centered_filename}'")
         current_image += 1
 
 def main(args):
